c4b43e0305bd74a3e40c25bfb55dc04668f8c4a8
Amandeep <amandeep.cs@gmail.com>
2017-07-04 22:50:36 +0530

    bare application

---
+++ terrific-server/src/main/java/com/gtt/terrific/TerrificServerApplication.java
@@ -0,0 +1,12 @@
+package com.gtt.terrific;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrificServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrificServerApplication.class, args);
+	}
+}
